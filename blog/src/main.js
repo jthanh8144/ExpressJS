03d998223acaf176c340642bab668786c7076006
jthanh8144 <thanhvo618@gmail.com>
2021-08-18 11:34:17 +0700

    post method

--- blog/src/main.js
+++ blog/src/main.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const port = 3000
+const express = require('express');
+// const morgan = require('morgan');
+const app = express();
+const port = 3000;
 const handlebars = require('express-handlebars');
 const path = require('path');
 
 // static file
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // template engine
 app.engine('hbs', handlebars({
@@ -27,11 +33,13 @@ app.get('/news', (req, res) => {
 });
 
 app.get('/search', (req, res) => {
+  console.log(req.query);
   res.render('search');
 });
 
 app.post('/search', (req, res) => {
-  res.render('search');
+  console.log(req.body);
+  res.send('');
 });
 
 app.listen(port, () => {
