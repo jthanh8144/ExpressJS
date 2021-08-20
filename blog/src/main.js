8df329b95a120aded45979cb7076219ef253eb0e
jthanh8144 <thanhvo618@gmail.com>
2021-08-20 10:41:50 +0700

    route part 1

--- blog/src/main.js
+++ blog/src/main.js
@@ -5,6 +5,8 @@ const port = 3000;
 const handlebars = require('express-handlebars');
 const path = require('path');
 
+const route = require('./routes');
+
 // static file
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -24,23 +26,8 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
-
-app.get('/search', (req, res) => {
-  console.log(req.query);
-  res.render('search');
-});
-
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('');
-});
+// routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ blog/src/routes/index.js
@@ -0,0 +1,21 @@
+function route(app) {
+    app.get('/', (req, res) => {
+        res.render('home');
+      });
+      
+    //   app.get('/news', (req, res) => {
+    //     res.render('news');
+    //   });
+      
+      app.get('/search', (req, res) => {
+        console.log(req.query);
+        res.render('search');
+      });
+      
+      app.post('/search', (req, res) => {
+        console.log(req.body);
+        res.send('');
+      });
+}
+
+module.exports = route;
