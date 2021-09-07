88f656687acb6ecc4439c1e48b90d1673349b15c
jthanh8144 <thanhvo618@gmail.com>
2021-09-07 16:28:55 +0700

    detail page

--- blog/src/app/models/Course.js
+++ blog/src/app/models/Course.js
@@ -4,6 +4,8 @@ const Course = new Schema({
     name: { type: String, default: '' },
     description: { type: String, default: '' },
     image: { type: String, default: '' },
+    slug:{ type: String, maxLength: 255 },
+    videoId:{ type: String, maxLength: 255 },
     createAt: { type: Date, default: Date.now },
     updateAt: { type: Date, default: Date.now },
 });
