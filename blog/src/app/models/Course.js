a826ab494893e338e1ad9774b64e198db92faf27
jthanh8144 <thanhvo618@gmail.com>
2021-09-12 14:24:40 +0700

    soft delete 1

--- blog/src/app/models/Course.js
+++ blog/src/app/models/Course.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
-
-mongoose.plugin(slug);
+const mongooseDelete = require('mongoose-delete');
 
 const Schema = mongoose.Schema;
 const Course = new Schema({
@@ -12,6 +11,13 @@ const Course = new Schema({
     videoID:{ type: String, required: true },
 }, {
     timestamps: true,
+});
+
+// Add plugins
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, { 
+    deletedAt : true,
+    overrideMethods: 'all' 
 });
 
 module.exports = mongoose.model('Course', Course);
