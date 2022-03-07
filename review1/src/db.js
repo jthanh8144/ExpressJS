319c252cc3b60558b34c1ce360701e770aff2ece
VoVanThanh <thanhvo618@gmail.com>
2022-03-07 12:57:13 +0700

    fix db connect

--- review1/src/db.js
+++ review1/src/db.js
@@ -1,11 +1,9 @@
 const { Client } = require('pg');
 
+const obj = require('./configdb.json');
+
 const connectObj = {
-    user: 'lagspzkjufqgqy',
-    host: 'ec2-34-231-183-74.compute-1.amazonaws.com',
-    database: 'dd3ja3cj1bflab',
-    password: '72fdf2b783eda912c460dfd4d8c61c759be9ffb435081f8d8165694aa402645b',
-    port: 5432,
+    ...obj,
     ssl: {
         require: true,
         rejectUnauthorized: false
